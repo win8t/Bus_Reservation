b2337949649067392b9df271981b47927a4d9ac3
malt1234 <malt.201410@gmail.com>
2024-04-30 15:37:16 +0800

    database

--- Booking.php
+++ Booking.php
@@ -45,7 +45,7 @@
         </div>
 
       </div>
-      <a href="#" class="login-button"><i class="bi bi-person-circle"></i> Login</a>
+      <a href="Login.php" class="login-button"><i class="bi bi-person-circle"></i> Login</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "db_bus";
+
+$con = new mysqli($servername, $username, $password, $database);
+
+?>
